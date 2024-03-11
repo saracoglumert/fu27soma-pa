83077357fc6cc9f7d904481babe9a0def0c692cd
MERT SARACOGLU <mert@saracoglu.com.tr>
2024-03-11 14:26:17 +0100

    update

--- res/init/server.sh
+++ res/init/server.sh
@@ -13,12 +13,6 @@ echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc 
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
-# dependencies
-pip3 install aries-cloudagent
-pip3 install aries_askar
-pip3 install indy_credx
-pip3 install indy_vdr
-
 # resources
 cd -L
 mkdir res
