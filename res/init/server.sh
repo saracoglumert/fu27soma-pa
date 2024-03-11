e7a408379cd0132ba10ee881ef1572309eba3d64
MERT SARACOGLU <mert@saracoglu.com.tr>
2024-03-11 14:26:52 +0100

    update

--- res/init/server.sh
+++ res/init/server.sh
@@ -1,4 +1,4 @@
-# Initialization
+# init
 echo 'en_US.UTF-8 UTF-8' >>  /etc/locale.gen
 locale-gen
 apt update -y
@@ -7,11 +7,9 @@ apt install git -y
 apt install curl -y
 apt install python3-pip -y
 curl -sSL https://get.docker.com/ | sh
-
-# set-up
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc 
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
 # resources
 cd -L
