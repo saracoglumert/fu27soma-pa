6e3786c770cbcc290c4af51a4aa4b6f66c22c843
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2024-03-11 04:14:38 +0100

    manage

--- scripts/manage.sh
+++ scripts/manage.sh
@@ -17,21 +17,21 @@ CONF_ROOTPASS="12345"
 CONF_TEMPLATE_PATH="/var/lib/vz/template/cache/debian-11-standard_11.7-1_amd64.tar.zst"
 CONF_TEMPLATE_URL='http://ftp.cn.debian.org/proxmox/images/system/debian-11-standard_11.7-1_amd64.tar.zst'
 
-read -p "This will take apprx. 10 minutes. Do you want to proceed? (y/n) " yn
-
-case $yn in 
-	y ) echo '';;
-	n ) echo '';
-		exit;;
-	* ) echo 'Invalid response.';
-		exit 1;;
-esac
-
 if ! test -f $CONF_TEMPLATE_PATH; then
   wget $CONF_TEMPLATE_URL -O $CONF_TEMPLATE_PATH
 fi
 
 BUILD () {
+  # Ask
+  read -p "This will take apprx. 10 minutes. Do you want to proceed? (y/n) " yn
+  case $yn in 
+    y ) echo '';;
+    n ) echo '';
+      exit;;
+    * ) echo 'Invalid response.';
+      exit 1;;
+  esac
+  
   # Create containers
   pct create $ID_SERVER $CONF_TEMPLATE_PATH --hostname "$HOSTNAME_SERVER" --memory "$CONF_MEMORY" --net0 name=eth0,bridge=vmbr0,firewall=1,gw=$CONF_GATEWAY,ip=$(echo ${IP_SERVER}${IP_EXT}),type=veth --storage local-lvm --rootfs local-lvm:8 --unprivileged 1 --ignore-unpack-errors --ostype debian --password="$CONF_ROOTPASS" --start 1 --features nesting=1
   pct create $ID_NODE1 $CONF_TEMPLATE_PATH --hostname "$HOSTNAME_NODE1" --memory "$CONF_MEMORY" --net0 name=eth0,bridge=vmbr0,firewall=1,gw=$CONF_GATEWAY,ip=$(echo ${IP_NODE1}${IP_EXT}),type=veth --storage local-lvm --rootfs local-lvm:8 --unprivileged 1 --ignore-unpack-errors --ostype debian --password="$CONF_ROOTPASS" --start 1 --features nesting=1
@@ -56,8 +56,10 @@ BUILD () {
 }
 
 START () {
+  # Start containers
   pct exec $ID_SERVER -- bash start
-
+  pct exec $ID_NODE1 -- bash start
+  pct exec $ID_NODE2 -- bash start
 }
 
 DESTROY () {
