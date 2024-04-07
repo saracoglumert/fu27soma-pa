e577b27892d239897131122bb2cd13e4c07f9b57
Mert Saracoglu <mert@saracoglu.com.tr>
2024-04-07 22:32:12 +0200

    update

--- dev/getredis.py
+++ dev/getredis.py
@@ -1,7 +1,7 @@
 from redis import Redis
 
 node1 = Redis.from_url(url="redis://root:12345@10.10.10.201:6379")
-node1 = Redis.from_url(url="redis://root:12345@10.10.10.202:6379")
+node2 = Redis.from_url(url="redis://root:12345@10.10.10.202:6379")
 
 #node1.ping()
 #node2.ping()
