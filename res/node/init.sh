ecfcc0202a1c5448a659ce340bd9f55244c3dc0d
Mert Saracoglu <mert@saracoglu.com.tr>
2024-04-08 00:55:28 +0200

    update

--- res/node/init.sh
+++ res/node/init.sh
@@ -1,14 +1,16 @@
 # Initialize OS
 echo 'en_US.UTF-8 UTF-8' >>  /etc/locale.gen
 locale-gen
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc 
+
+# Updates and dependencies
 apt update -y
 apt upgrade -y
 apt install git -y
 apt install curl -y
 apt install python3-pip -y
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc 
 
 # Install aca-py and dependencises
 pip3 install aries-cloudagent
--- res/server/init.sh
+++ res/server/init.sh
@@ -1,15 +1,23 @@
 # Initialize OS
 echo 'en_US.UTF-8 UTF-8' >>  /etc/locale.gen
 locale-gen
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc 
+
+# Initialize apt repos
+export DEBIAN_FRONTEND=noninteractive
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
+dpkg -i mysql-apt-config_0.8.29-1_all.deb
+
+# Updates and dependencies
 apt update -y
 apt upgrade -y
 apt install git -y
 apt install curl -y
 apt install python3-pip -y
 apt install lsb-release -y
-echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc 
+apt install mysql-server -y
 
 # Install Docker
 curl -4sSL https://get.docker.com/ | sh
@@ -21,13 +29,6 @@ cd res
 git clone https://github.com/bcgov/von-network
 git clone https://github.com/bcgov/indy-tails-server.git
 cd ..
-
-# Install MySQL
-export DEBIAN_FRONTEND=noninteractive
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
-dpkg -i mysql-apt-config_0.8.29-1_all.deb
-apt update
-apt install mysql-server -y
 
 # Setup MySQL
 mysql -uroot -e "CREATE USER 'root'@'%' IDENTIFIED BY '12345'"
